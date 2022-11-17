4fbb4bfb427acfeb9149ce647950a566e805c446
jennyyoo <jennyyoo@jennys-air-2.dhcp.wustl.edu>
2022-11-17 11:54:14 -0600

    should i get stanleys

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,56 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
-
-
+	private int day;
+	private int month;
+	private int year;
+	/*private int timeHour;
+	private int timeMin; */
+	public Date(int m, int d, int y) {
+		month = m;
+		day = d;
+		year = y;
+		/*timeHour = h;
+		timeMin = min;*/
+	}
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+	public String toString() {
+		return month + "/" + day + "/" + year;
+	}
+	
     public static void main(String[] args) {
-
+    	LinkedList<Date> list = new LinkedList<Date>();
+    	Date christmas = new Date (12, 25, 2022);
+    	Date christmasEve = new Date (12, 24, 2022);
+    	list.add(christmas);
+    	list.add(christmasEve);
+    	list.add(christmas);
+    	HashSet<Date> set = new HashSet<Date>();
+    	set.add(christmas);
+    	set.add(christmasEve);
+    	set.add(christmas);
+    	System.out.println(set);
+    	System.out.println(list);
+    	System.out.println(christmas);
+    	System.out.println(christmas.equals(christmasEve));
     }
+
+	
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,42 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
-
+	private int timeHour;
+	private int timeMin; 
+	private boolean militaryTime;
+	public Time(int h, int min, boolean timeFormat) {
+		timeHour = h;
+		timeMin = min;
+		militaryTime = timeFormat;
+	}
+	public int hashCode() {
+		return Objects.hash(militaryTime, timeHour, timeMin);
+	}
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return militaryTime == other.militaryTime && timeHour == other.timeHour && timeMin == other.timeMin;
+	}
+	public String toString() {
+		if (militaryTime == true) {
+			return timeHour + ":" + timeMin;
+		} else {
+			return (timeHour-12) + ":" + timeMin;
+		}
+	}
+	
 	public static void main(String[] args) {
- 
-    	
+		Time check = new Time(23,11,false);
+		Time checkTwo = new Time(23,59,false);
+		System.out.println(check);
+		System.out.println(check.equals(checkTwo));		
     }
 
 }
